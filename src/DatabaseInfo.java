ad084c0a10fa90bcdfba93725d1120defc24eba9
Jiakuan Li <jiak94@uga.edu>
2015-07-24 18:00:05 -0400

    build servlet

--- src/DatabaseInfo.java
+++ src/DatabaseInfo.java
@@ -1,5 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
 /**
  * Created by Jiakuan on 7/23/15.
  */
-public class DatabaseInfo {
+public class DatabaseInfo extends HttpServlet {
+    public DatabaseInfo() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        response.setHeader("Cache-control", "no-cache, no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "-1");
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Max-Age", "86400");
+
+    }
 }
